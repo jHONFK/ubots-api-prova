dd737a9604b6d61181ef04403c5ed507f03c9002
Claudio Reichert <58773731+jHONFK@users.noreply.github.com>
2021-05-04 07:54:03 -0300

    Delete src directory

--- src/main/java/com/filmes/domain/FilmeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.filmes.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FilmeRepository extends CrudRepository<Filme,Long> {
-    Iterable<Filme> findByAvaliado(boolean avaliado);
-}
